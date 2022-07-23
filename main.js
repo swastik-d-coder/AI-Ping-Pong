a35b689792819a1652ee9aff56ba8d5e0d829948
swastik-d-coder <102283282+swastik-d-coder@users.noreply.github.com>
2022-07-23 17:45:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,10 +23,24 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+    video = createCapture(VIDEO);
+    video.size(700,600);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+}
+
 function draw(){
+
+  image(video, 0, 0, 350, 350);
+        objectDetector.detect(video, gotResults);
 
  background(0); 
 
